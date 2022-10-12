3fc1d3d0b0b5a14cc6d0e2ffb20f740d1bee56ec
KruKuma <kruthai2540@gmail.com>
2022-10-12 23:17:11 +0200

    Add class object for Staff, NonTeaching and TeachingStaff

---
+++ src/main/java/com/example/eclipselink/entity/NonTeachingStaff.java
@@ -0,0 +1,34 @@
+package com.example.eclipselink.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue( value = "NS" )
+
+public class NonTeachingStaff extends Staff {
+	
+	private String areaexpertise;
+
+	public NonTeachingStaff(int sid, String sname, String areaexpertise) {
+		super(sid, sname);
+		this.areaexpertise = areaexpertise;
+	}
+	
+	public NonTeachingStaff() {
+		super();
+	}
+
+	public String getAreaexpertise() {
+		return areaexpertise;
+	}
+
+	public void setAreaexpertise(String areaexpertise) {
+		this.areaexpertise = areaexpertise;
+	}
+
+	@Override
+	public String toString() {
+		return "NonTeachingStaff [areaexpertise=" + areaexpertise + "]";
+	}
+}
